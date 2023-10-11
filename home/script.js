66fcd68eab5211fd8fa99d0dcadb81a04498d92f
tavares <60970454+gustavolnx@users.noreply.github.com>
2023-10-11 14:30:00 -0300

    weather 0.2
    mudanças no js para pegar data e hora automaticamente baseada na hora do pc
    icones da temperatura agora se alteram também automaticamente utilizando o msm ícone que o XML

--- home/script.js
+++ home/script.js
@@ -16,8 +16,35 @@ function importarXML(xml) {
   var euro = xmlDoc.querySelector("euro").textContent;
   var dolar = xmlDoc.querySelector("dollar").textContent;
 
+  var imgVar2 = xmlDoc.querySelector("imgVar2").textContent;
+  var temperaturaImg = document.querySelector(".temperatura-img");
+  temperaturaImg.style.backgroundImage = "url(" + imgVar2 + ")";
+  console.log(imgVar2);
+
   // Substituir os valores no HTML
   document.querySelector(".temperatura a").textContent = temperatura;
   document.querySelector(".euro a").textContent = euro;
   document.querySelector(".dolar a").textContent = dolar;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Seleciona o elemento com a classe "data"
+  var dataElement = document.querySelector(".data");
+
+  // Obtém a data atual
+  var dataAtual = new Date();
+
+  // Formata a data no formato desejado (04 de Outubro de 2023)
+  var options = { year: "numeric", month: "long", day: "numeric" };
+  var dataFormatada = dataAtual.toLocaleDateString("pt-BR", options);
+
+  // Define o conteúdo do elemento para a data formatada
+  dataElement.querySelector("a").textContent = dataFormatada;
+
+  var horaElement = document.querySelector(".hora");
+  var horaFormatada = dataAtual.toLocaleTimeString("pt-BR", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  horaElement.querySelector("a").textContent = horaFormatada;
+});
